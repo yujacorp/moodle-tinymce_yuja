5ed7c87f6167fb961c02844409d9dca6c9244bd5
lieuanna <anna.lieu@mail.utoronto.ca>
2021-09-03 16:15:11 -0400

    [#179422957] Implement privacy API for tinymce plugin
    - the privacy api indicates the LTI data that the plugin sends to YuJa

---
+++ classes/privacy/provider.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * YuJa Plugin for Moodle Tinymce
+ * @package    tinymce_yuja
+ * @subpackage yuja
+ * @copyright  2016 YuJa
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tinymce_yuja\privacy;
+use \core_privacy\local\metadata\collection;
+
+/**
+ * The provider class for implementing the Privacy API for YuJa Moodle Client
+ *
+ * This documents which moodle data this plugin can access, and what it will be used for.
+ * This plugin does store personal user data.
+ */
+class provider implements \core_privacy\local\metadata\provider {
+
+    /**
+     * Returns meta data about this system.
+     * @param collection $collection the collection to add the information to
+     * @return collection
+     */
+    public static function get_metadata(collection $collection) : collection {
+        $collection->add_external_location_link('lti_client', [
+            'user_id' => 'privacy:metadata:lti_client:user_id',
+            'user_fullname' => 'privacy:metadata:lti_client:user_fullname',
+            'course_id' => 'privacy:metadata:lti_client:course_id',
+            'course_shortname' => 'privacy:metadata:lti_client:course_shortname',
+            'course_fullname' => 'privacy:metadata:lti_client:course_fullname',
+            'user_idnumber' => 'privacy:metadata:lti_client:user_idnumber',
+            'user_username' => 'privacy:metadata:lti_client:user_username',
+            'user_family' => 'privacy:metadata:lti_client:user_family',
+            'user_given' => 'privacy:metadata:lti_client:user_given',
+            'user_email' => 'privacy:metadata:lti_client:user_email',
+            'roles' => 'privacy:metadata:lti_client:roles',
+            'moodle_version' => 'privacy:metadata:lti_client:moodle_version',
+            'course_idnumber' => 'privacy:metadata:lti_client:course_idnumber',
+            'course_startdate' => 'privacy:metadata:lti_client:course_startdate',
+            'instance_guid' => 'privacy:metadata:lti_client:instance_guid'
+        ], 'privacy:metadata:lti_client');
+
+        return $collection;
+    }
+}
